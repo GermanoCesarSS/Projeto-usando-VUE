61f46a6cee630f63771fb5d33b7c2a045a4f6e06
GermanoC <germanosantos.cesar@gmail.com>
2023-08-10 20:31:14 -0300

    Script concluido

--- src/Multiplicacao.vue
+++ src/Multiplicacao.vue
@@ -4,12 +4,12 @@
     <form @submit.prevent="multiplicar()">
       <div class="col">
         <label for="n1">1° Número</label>
-        <input type="number" name="n1" id="n1"
+        <input type="number" :disabled="campoDesabilitado" name="n1" id="n1"
         class="form-control" v-model="n1"/>
       </div>
       <div class="col mt-3">
         <label for="n2">1° Número</label>
-        <input type="number" name="n2" id="n2"
+        <input type="number" :disabled="campoDesabilitado" name="n2" id="n2" 
         class="form-control" v-model="n2"/>
       </div>
       <div class="col text-center mt-3 d-grid gap-2">
@@ -21,7 +21,7 @@
         class="btn btn-warning">Voltar</router-link>
       </div>
     </form>
-    <h3 v-if="multiplicacao != null" >
+    <h3 v-if="multiplicacao != null" class="mt-3 text-center" >
       O produto entre {{ n1 }} e {{ n2 }} é {{ multiplicacao }}
     </h3>
   </div>
@@ -33,14 +33,17 @@
   const n2 = ref(0)
 
   const multiplicacao = ref(null) 
+  const campoDesabilitado = ref(false)
 
   function multiplicar(){
     multiplicacao.value = n1.value * n2.value
+    campoDesabilitado.value = true
   }
 
   function limparReferencias(){
     n1.value = n2.value = 0
     multiplicacao.value = null
+    campoDesabilitado.value = false
   }
   
 </script>
